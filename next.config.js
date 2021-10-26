0b858ebbe7cb818dc1a3ccd1e82f44c89bed19b7
Chris Hannaby <chris.hannaby@netlify.com>
2021-10-25 19:35:54 -0600

    use zones for i18n

--- next.config.js
+++ next.config.js
@@ -1,3 +1,21 @@
+const { IS_DEFAULT_LOCALE, LOCALE } = process.env;
+
 module.exports = {
   reactStrictMode: true,
-}
+  basePath: IS_DEFAULT_LOCALE ? "/" : `/${LOCALE}`,
+  async rewrites() {
+    if (IS_DEFAULT_LOCALE) {
+      return [
+        {
+          source: "/de",
+          destination: "https://next-i18n-zones-de.netlify.app/de",
+        },
+        {
+          source: "/de/:path*",
+          destination: "https://next-i18n-zones-de.netlify.app/de/:path*",
+        },
+      ];
+    }
+    return [];
+  },
+};
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,10 @@
-export async function getStaticProps(context) {
+import styles from "../../styles/Home.module.css";
+
+export async function getStaticProps({ params }) {
   return {
-    props: {}, // will be passed to the page component as props
+    props: {
+      id: params.id,
+    }, // will be passed to the page component as props
   };
 }
 
@@ -8,6 +12,12 @@ export async function getStaticPaths() {
   return { paths: [], fallback: "blocking" };
 }
 
-export default function Home() {
-  return <div>Hi</div>;
+export default function Home({ id }) {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Welcome to {id}</h1>
+      </main>
+    </div>
+  );
 }
